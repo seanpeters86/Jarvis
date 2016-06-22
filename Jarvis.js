1fdf5a9df7fb464751b0008617990ae8865cbd3c
seanpeters86 <sean.peters86@icloud.com>
2016-06-22 09:29:55 -0500

    Update Jarvis.js

--- Jarvis.js
+++ Jarvis.js
@@ -1,10 +1,12 @@
-// Jarvis Discord Bot -- Written by Sean Peters
+/*
+File: Jarvis.js
+Author: Sean Peters
+Created: 06/22/2016
+Description: Main Bot File
+*/
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 require('String.prototype.startsWith'); // used to parse complicated messages
-
-// username array for !superadd
-//var usernames = ["Zurchi","Republic (Sean)"];
 
 // begin main bot
 bot.on("message", function(message) {
@@ -185,45 +187,5 @@ bot.on("message", function(message) {
     else if (input === "!RANDOM" || input === "#ITSRANDOM") {
         bot.sendMessage(message, "It's never random. Molo is a cheater.");
     }
-    /*
-    // !superadd Zurchi Paladin
-    else if(input.startsWith("!SUPERADD")){
-      var roleName = message.content.split(" ");
-      var server = message.channel.server;
-      var roles = message.channel.server.roles;
-      if(usernames.indexOf(roleName[1],usernames) > -1){
-        var user = message.channel.server.members.get("username",roleName[1]).id;
-        bot.sendMessage(message,"Username found!");
-      } else {
-        bot.sendMessage(message,"Username not found");
-      }
-      //var role = roles.get("name",roleName[2]).id;
-      //bot.addMemberToRole(user,role);
-      //bot.reply("Added Paladin to Zurchi");
-    }
-    */
-    // question
-    /*
-    else if(input.startsWith("!QUESTION")){
-      var random = Math.floor((Math.random() * 8) + 1);
-      if(random == 1){
-        bot.reply(message, "It is certain.");
-      }else if(random == 2){
-        bot.reply(message, "Without a doubt.");
-      }else if(random == 3){
-        bot.reply(message, "Most likely.");
-      }else if(random == 4){
-        bot.reply(message, "Yes.");
-      }else if(random == 5){
-        bot.reply(message, "No.");
-      }else if(random == 6){
-        bot.reply(message, "My sources say no.");
-      }else if(random == 7){
-        bot.reply(message, "Ask again later.");
-      }else{
-        bot.reply(message, "Very doubtful");
-      }
-    }
-    */
 });
 bot.loginWithToken("XXXXX");
