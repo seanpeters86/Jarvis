13123765d44a19701345f613c4fef29904bccb40
seanpeters86 <sean.peters86@icloud.com>
2017-05-07 20:02:11 -0400

    adding new field

--- jarvis.js
+++ jarvis.js
@@ -1056,9 +1056,9 @@ bot.on("error", (m) => console.log("[error]", m));
 //bot.on("raw", (m) => console.log("[raw]", m));
 
 bot.on("serverNewMember", function(botter,msg,suffix) {
-  userid = msg.toString().substring(2, 18);
-  console.log("User: " + userid + " joined the server.");
-  bot.sendMessage(userid, "Welcome to the server! Read the welcome channel pl0x.");
+  userid = msg.user.toString().substring(2, 18);
+  console.log("User: " + userid + " joined the server." + " " + botter);
+  //bot.sendMessage(userid, "Welcome to the server! Read the welcome channel pl0x.");
 });
 
 bot.on("disconnected", function() {
