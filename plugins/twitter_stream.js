5aea10feb4c4fcfb20a7f2e39119ed386fb5ad44
seanpeters86 <sean.peters86@icloud.com>
2017-05-17 19:34:19 -0400

    trying to get export working

--- plugins/twitter_stream.js
+++ plugins/twitter_stream.js
@@ -13,7 +13,7 @@ var client = new Twitter({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-exports.function get_tweet() {
+function get_tweet() {
   var channel, content = "";
   var tweet = [channel, content];
   client.stream('statuses/filter', {follow: '753346461849354240,27171880,174307074,365528029,2420931980,3065618342', filter_level: 'low', language: 'en'}, function(stream){
@@ -32,3 +32,5 @@ exports.function get_tweet() {
     });
   });
 }
+
+export { get_tweet() };
