37e9fe06a6c07c6ecbf61b11042009c565112b7c
seanpeters86 <sean.peters86@icloud.com>
2017-05-18 01:05:37 -0400

    adding declaration

--- plugins/commands.js
+++ plugins/commands.js
@@ -1,7 +1,11 @@
-exports.classes = ["Mage", "Death", "Druid", "Hunter", "Demon", "Monk", "Paladin", "Priest", "Rogue", "Shaman", "Warlock", "Warrior"];
-exports.channelRoles = ["Developers", "Music", "Healers", "Theorycrafting", "Overwatch", "HoTs", "Memes"];
-exports.channelList = ["memes", "developers", "music", "raiding", "guild-chat", "senior-raiders", "officers", "overwatch", "theorycrafting", "welcome", "healing", "hunters", "hots"];
+var classes = ["Mage", "Death", "Druid", "Hunter", "Demon", "Monk", "Paladin", "Priest", "Rogue", "Shaman", "Warlock", "Warrior"];
+var channelRoles = ["Developers", "Music", "Healers", "Theorycrafting", "Overwatch", "HoTs", "Memes"];
+var channelList = ["memes", "developers", "music", "raiding", "guild-chat", "senior-raiders", "officers", "overwatch", "theorycrafting", "welcome", "healing", "hunters", "hots"];
 var planfortheweek = "Time to kill Elisande! We will be extending to work on her this week.";
+
+exports.classes = classes;
+exports.channelRoles = channelRoles;
+exports.channelList = channelList;
 
 var balance = [process.env.BALANCE_DATE, process.env.BALANCE];
 
