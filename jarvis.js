7dc3ea243c738a45985ba50d4642ba13407a6ae5
seanpeters86 <sean.peters86@icloud.com>
2017-05-18 15:51:21 -0400

    adding stuff

--- jarvis.js
+++ jarvis.js
@@ -88,6 +88,37 @@ bot.on('message', message => {
 		message.delete();
 		message.member.send("This language: ```" + input + "``` is not allowed in this server.")
 	}
+  else if (intput === "!TEST") {
+        message.channel.send({embed: {
+      color: 3447003,
+      author: {
+        name: bot.user.username,
+        icon_url: bot.user.avatarURL
+      },
+      title: 'This is an embed',
+      url: 'http://google.com',
+      description: 'This is a test embed to showcase what they look like and what they can do.',
+      fields: [
+        {
+          name: 'Fields',
+          value: 'They can have different fields with small headlines.'
+        },
+        {
+          name: 'Masked links',
+          value: 'You can put [masked links](http://google.com) inside of rich embeds.'
+        },
+        {
+          name: 'Markdown',
+          value: 'You can put all the *usual* **__Markdown__** inside of them.'
+        }
+      ],
+      timestamp: new Date(),
+      footer: {
+        icon_url: bot.user.avatarURL,
+        text: '© Example'
+      }
+    }});
+  }
 	// fistmas
 	else if (input === "!FISTMAS") {
 		message.channel.send(commands.fistmas[admin.random(7)]);
@@ -203,7 +234,7 @@ bot.on('message', message => {
 	else if ((input.startsWith("!ADDROLE") || input.startsWith("!ADD") || input.startsWith("!JOIN")) && !(message.channel.isPrivate)) {
 		//role = admin.get_role(parsed, roles);
     //console.log(role);
-    user.addRole('Hunter');
+    member.addRole('Hunter');
 		if (role) {
 			member.addRole('Hunter');
 			message.reply("Added " + parsed[1] + " role.");
