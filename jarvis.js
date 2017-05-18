0cabf110f6cfca018d207fcb1ed399f5718ddd26
seanpeters86 <sean.peters86@icloud.com>
2017-05-18 17:52:10 -0400

    fix typo

--- jarvis.js
+++ jarvis.js
@@ -287,7 +287,7 @@ bot.on('message', message => {
 				//console.log(rank);
 				if (wclObject[1] != 0) {
 					var rankObject = wcl.get_rank(rank, wclObject[2], input, parsed);
-          var art = wcl.get_art(rank,wclObject[8]);
+          var art = wcl.get_art(rank,rankObject[8]);
           console.log(art);
 					if (!(input.includes("-P"))) {
 						message.member.send(rankObject);
