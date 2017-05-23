1d9d88dead189242e7be55c47487ca8c8a30d5bd
seanpeters86 <sean.peters86@icloud.com>
2017-05-23 13:11:57 -0400

    changing weeks

--- plugins/affixes.js
+++ plugins/affixes.js
@@ -18,7 +18,7 @@ module.exports = {
 		var secondDate = d;
 		var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneday)));
 		var weeks = Math.floor(diffDays / 7);
-		var week = (weeks % 8) + 2;
+		var week = (weeks % 8);
 		var nextweek;
 		if (week == 9) {
 			nextweek = 1;
