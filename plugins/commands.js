143a71838aa987d01c2838e969ca3f0994061dfe
seanpeters86 <sean.peters86@icloud.com>
2017-06-20 12:41:43 -0400

    fixing another typo

--- plugins/commands.js
+++ plugins/commands.js
@@ -85,7 +85,7 @@ exports.responses = {
 	"!SOURCE": "Here is the link to my public source code: <https://github.com/seanpeters86/Jarvis>",
 	"!STATS": "Here is the stat weights/priorities sheet: <https://docs.google.com/document/d/1gIOSfzrSHCd_j_ZUXl0gasuLprjCqzR5hgaVasX7ow4/edit?usp=sharing>",
 	"!STAYCLASSY": "The following are the race/class combos we still need:\nTauren(Priest)\nTrolls(Warlock)\nUndead(Hunter)\nGoblins(DK, Rogue, Warlock, Priest)\nPandaren(Mage, Priest)",
-	"!TOOLKIT": "Here is the video toolkot: <https://www.dropbox.com/sh/3ly14sy3k4633j3/AADgUuFn8rP3ZwDC6P_Tyqoaa?dl=0>",
+	"!TOOLKIT": "Here is the video toolkit: <https://www.dropbox.com/sh/3ly14sy3k4633j3/AADgUuFn8rP3ZwDC6P_Tyqoaa?dl=0>",
 	"!TOMB": "Here is the document for ToS boss strats: <https://docs.google.com/document/d/16WugrU4q2zhn2OZrMIsXCs9xBlB_uLr-BffjQNa5KIQ/edit?usp=sharing>",
 	"!TOS": "Here is the document for ToS boss strats: <https://docs.google.com/document/d/16WugrU4q2zhn2OZrMIsXCs9xBlB_uLr-BffjQNa5KIQ/edit?usp=sharing>",
 	"!UTILITY": "Here is the Legion Defensive/Utility Spell List: <https://docs.google.com/spreadsheets/d/1x6m0C6zJHVsEh63bgUgkQBKyLnkxYGeK3LM5JiVESzY/edit?usp=sharing>",
