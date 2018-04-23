76463fc7fedeb5d22bf529eeffdbf516635f012a
seanpeters86 <sean.peters86@icloud.com>
2018-04-23 13:43:34 -0500

    remove and

--- jarvis.js
+++ jarvis.js
@@ -101,7 +101,7 @@ bot.on('message', message => {
 	}
 	// @here test
 	else if ((input.startsWith("!HERE") || input.startsWith("@HERE")) && ((message.channel.id == "230827366740393984") || (message.channel.id == "180011389115564032"))) {
-		if (!message.author.bot &&) {
+		if (!message.author.bot) {
 			roleid = roles.find("name", "Officers")
 			console.log(roleid.members)
 			console.log(roleid.members.has(message.author.id))
